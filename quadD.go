ecc1c7bcf399e315c9d42e6d6fe2e4ef0ffdff38
mkotkov <maximkotkov@gmail.com>
2023-05-27 13:46:16 +0300

    D

---
+++ quadD.go
@@ -0,0 +1,24 @@
+package piscine
+
+import "fmt"
+
+func QuadD(x, y int) {
+	if x > 0 && y > 0 {
+		for i := 0; i < y; i++ {
+			for j := 0; j < x; j++ {
+				if i == 0 && (j == 0 || j == x-1) {
+					fmt.Print("A")
+				} else if i == y-1 && (j == 0 || j == x-1) {
+					fmt.Print("A")
+				} else if i == 0 || i == y-1 || j == 0 || j == x-1 {
+					fmt.Print("B")
+				} else {
+					fmt.Print(" ")
+				}
+			}
+			fmt.Println()
+		}
+	} else {
+		fmt.Println("The dimensions must be positive.")
+	}
+}
