32448b2753e82811dd35e541bd99615e1ef7f62c
Sandra Ratasepp <sandraratasepp@Sandras-MacBook-Air-2.local>
2023-05-27 10:48:02 +0300

    quadA

--- quadA.go
+++ quadA.go
@@ -6,17 +6,21 @@ import "strings"
 func QuadA(x,y int) {
 
 	if x > 0 && y > 0 {
-		for i := 0 ; i < x-2 ; i++ {
+
+		if x == 1 {
+			fmt.Println ("o")
+		} else {
 			fmt.Println ("o" + strings.Repeat("-", (x-2)) + "o")
 		}
 		for i := 0 ; i < y-2 ; i++ {
 			fmt.Println ("|" + strings.Repeat(" ", (x-2)) + "|")
 		}
+		}
+	
 	}
 		
-}
 
 func main() {
-	QuadA(5,1)
+	QuadA(1,1)
 }
 
